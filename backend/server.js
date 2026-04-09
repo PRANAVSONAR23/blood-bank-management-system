cf706b20b4823595ac719813a7af2b15e7a88f34
PRANAVSONAR23 <pranavsonar2311@gmail.com>
2026-04-09 16:22:04 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"], // Allow both ports
-  credentials: true,
+  origin: "*",
+  credentials: false,
 }));
 
 app.use('/api/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
